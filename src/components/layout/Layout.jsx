3404fddb2f42bd998c36873155b37035fb54d2bc
Wils0nCastill0 <151974275+Wils0nCastill0@users.noreply.github.com>
2024-12-16 14:49:59 -0300

    Arreglos

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import {
   Box,
-  VStack,
-  Icon,
-  Text, 
   Flex,
-  IconButton,
-  useColorModeValue,
   useDisclosure
 } from '@chakra-ui/react';
-import { useLocation, useNavigate, Outlet } from 'react-router-dom';
-import {
-  LayoutDashboard,
-  ShoppingCart,
-  Package,
-  Settings,
-  Menu,
-  Home,
-  Users,
-  FileText,
-} from 'lucide-react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/slices';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { CollapsibleSidebar } from './CollapsibleSidebar';
 
@@ -31,13 +14,9 @@ export const Layout = () => {
   return (
     <Flex h="100vh">
       <CollapsibleSidebar isOpen={isOpen} onToggle={onToggle} />
-      <Box
-        flex="1"
-        ml={isOpen ? '240px' : '60px'}
-        transition="margin-left 0.3s"
-      >
-        <Navbar onMenuClick={onToggle} userName={''} />
-        <Box p={4}>
+      <Box flex="1" ml={isOpen ? '240px' : '60px'} transition="margin-left 0.3s">
+        <Navbar onMenuClick={onToggle} isSidebarOpen={isOpen} />
+        <Box mt="60px" p={4}>
           <Outlet />
         </Box>
       </Box>
